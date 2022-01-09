77206c7e5a46811ef2a86d92ffc42861d3a9a382
CloudHill <rh.cloudhill@gmail.com>
2022-01-09 14:19:08 +0700

    setup react frontend

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(
+    <App />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
---
+++ app/javascript/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Task from './Task';
+
+export default () => (
+  <div>
+    <h1>Test</h1>
+    <p>Hello</p>
+  </div>
+);
